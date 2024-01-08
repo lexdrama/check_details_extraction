e7b9b96b985e05a42c11ccf9f493d1d29e68c4e0
anshumannec <anshuman.tiwari@india.nec.com>
2024-01-08 04:22:08 +0000

    added file for excel conversion

---
+++ with_ocr/codes/image_to_excel.py
@@ -0,0 +1,19 @@
+
+import os 
+import csv
+
+
+def create_excel_file(folder_path):
+
+    with open("phase1.csv", 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        csv_writer.writerow(['filename'])
+
+        for filename in os.listdir(folder_path):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                csv_writer.writerow([filename])
+
+folder_path = 'phase1'
+
+create_excel_file(folder_path)
